3e69ab98f3a7055695592e8693a617cd29d674e0
Peace Daniel <peacedaniel612@gmail.com>
2024-05-19 08:38:33 +0100

    Worked on header and figure for Hangman

---
+++ Hangman/src/HangmanHome.jsx
@@ -0,0 +1,16 @@
+const HangmanHome = () => {
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="card2">
+                    <img src="" alt="Hangman image"></img>
+                    <h3>Questions:</h3>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+
+export default HangmanHome;
